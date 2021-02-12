92ecdc390305afe180fc498619cf18167adaa1a5
Jack <jackpmalone01@gmail.comgit config --global user.name jackMalone2001>
2021-02-12 21:50:19 +0000

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'RedditProject';
+    articles: Article[];
+
+    constructor() {
+        this.articles = [
+            new Article('Angular', 'http://angular.io', 5),
+            new Article('Google', 'http://google.ie', 3),
+            new Article('Reddit', 'https://reddit.com', 100)
+        ];
+    }
+
+    addArticle(title: HTMLInputElement, link: HTMLInputElement): Boolean {
+        if (title.value != "" && link.value != "") {
+            this.articles.push(new Article(title.value, link.value, 0));
+
+            title.value = "";
+            link.value = "";
+
+            return false;
+        }
+        else return false;
+    }
+
+    sortedArticles(): Article[] {
+        return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+    }
 }
---
+++ src/app/article/article.model.ts
@@ -0,0 +1,30 @@
+export class Article {
+    votes: number;
+    title: string;
+    link: string;
+
+    constructor(title: string, link: string, votes?: number) {
+        this.title = title;
+        this.link = link;
+        this.votes = votes || 0;
+    }
+
+    voteUp() {
+        this.votes++;
+    }
+
+    voteDown() {
+        this.votes--;
+    }
+
+    domain(): string {
+        try {
+            const link: string = this.link.split('//')[1];
+            return link.split('/')[0];
+        }
+
+        catch (err) {
+            return null;
+        }
+    }
+}
